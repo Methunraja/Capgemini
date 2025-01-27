857e4677f478b8071689e2a88ac94cd49de1adbe
Rajendran Nadar <methunraja.rajendran-nadar@capgemini.com>
2025-01-27 14:39:01 +0530

    initial commit

---
+++ src/main/java/com/Capgemini/BenchApps/BenchAppsApplication.java
@@ -0,0 +1,14 @@
+package com.Capgemini.BenchApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchAppsApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/com/Capgemini/BenchApps/Pojo/package-info.java
@@ -0,0 +1,1 @@
+package com.Capgemini.BenchApps.Pojo;
